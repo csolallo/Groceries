dd4de1dd5225633350463c2d150d0b6436fd559c
Carlos Sola-Llonch <csolallo@gmail.com>
2025-08-15 09:46:38 -0700

    driver now expects a parameter

--- scripts/driver.rb
+++ scripts/driver.rb
@@ -3,20 +3,30 @@ require 'set'
 require_relative 'lib/input_parser'
 require_relative 'lib/keep'
 
-Dotenv.load
+def main(arv)
+  Dotenv.load
+  
+  credentials = Groceries::user_credentials(
+    for: ENV['USER_TO_IMPERSONATE'], 
+    api_key: ENV['GOOGLE_APPLICATION_CREDENTIALS']
+  )
 
-credentials = Groceries::user_credentials(
-  for: ENV['USER_TO_IMPERSONATE'], 
-  api_key: ENV['GOOGLE_APPLICATION_CREDENTIALS']
-)
+  list = Groceries::List.new(credentials, ENV['GROCERY_LIST_NAME'])
+  uniques = Set.new(list.get_items)
 
-list = Groceries::List.new(credentials, ENV['GROCERY_LIST_NAME'])
-uniques = Set.new(list.get_items)
+  # TODO actually get items from input file
+  new_items = ['Tomatoes', 'Apples', 'Bananas']
 
-# TODO actually get items from input file
-new_items = ['Tomatoes', 'Apples', 'Bananas']
+  new_items.collect { |item| uniques << item }
+  puts uniques
 
-new_items.collect { |item| uniques << item }
-puts uniques
+  list.save(uniques.to_a).share_with(ENV['USER_TO_INVITE'])  
+end
 
-list.save(uniques.to_a).share_with(ENV['USER_TO_INVITE'])
+if ARGV.size != 2
+  STDERR.puts 'grocery list file path required'
+  exit(-1)
+end
+
+ARGV.shift
+main(ARGV)
