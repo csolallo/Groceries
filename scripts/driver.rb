b91f2e066ee8b596a15b0afc2d6eb01920aa2833
Carlos Sola-Llonch <csolallo@gmail.com>
2025-08-14 22:17:45 -0700

    basic writing and reading of Keep notes

--- scripts/driver.rb
+++ scripts/driver.rb
@@ -3,4 +3,15 @@ require_relative 'lib/input_parser'
 require_relative 'lib/keep'
 
 Dotenv.load
-puts "Key Path: #{ENV['GOOGLE_APPLICATION_CREDENTIALS']}"
+
+credentials = Groceries::user_credentials(
+  for: ENV['USER_TO_IMPERSONATE'], 
+  api_key: ENV['GOOGLE_APPLICATION_CREDENTIALS']
+)
+
+list = Groceries::List.new(credentials, ENV['GROCERY_LIST_NAME'])
+list.save(['Tomatoes', 'Apples', 'Bananas'])
+
+list = Groceries::List.new(credentials, ENV['GROCERY_LIST_NAME'])
+items = list.get_items
+puts items
--- scripts/lib/keep.rb
+++ scripts/lib/keep.rb
@@ -1,7 +1,96 @@
 require 'googleauth'
 require 'google/apis/keep_v1'
 
-# a minimal wrapper for the (minimal) google keep api
-class Keep
-  
+# making the Keep api more ruby-like
+
+Google::Apis::KeepV1::ListContent.define_method(:<<) do |args|
+  if args.is_a?(Google::Apis::KeepV1::ListItem)
+    item = args
+  elsif args.respond_to?(:to_s)
+    text_content = Google::Apis::KeepV1::TextContent.new
+    text_content.text = args.to_s
+
+    item = Google::Apis::KeepV1::ListItem.new    
+    item.text = text_content 
+    item.checked = false
+  end
+
+  self.list_items ||= []
+  self.list_items << item  
+end
+
+module Groceries
+  Keep = Google::Apis::KeepV1
+
+  # a minimal wrapper for the (minimal) google keep api 
+  class List
+    def initialize(authorizer, name="Groceries")
+      @authorizer = authorizer
+      @list_name = name
+    end
+
+    def get_items
+      with_authorized_service do |s|
+        response = s.list_notes
+        note = response.notes.find { |n| n.title == @list_name }
+        items = []
+        unless note.nil?
+          # TODO validate that the not is actually a list
+          items = note.body.list.list_items.inject([]) { |lst, item| lst << item.text.text} 
+        end
+        return items
+      end
+    end
+    
+    def save(items)
+      with_authorized_service do |s|
+        # for us, type will always be list
+        create_note(type: :list, title: @list_name) do |n|
+          items.collect { |item| n.body.list << item }
+          s.create_note(n)
+        end
+      end
+    end
+
+    private 
+
+    # create_note serves as a reminder that we're actually creating a Keep not
+    def create_note(**args)
+      note = Keep::Note.new
+      note.title = args[:title] unless args[:title].nil?
+      note.body = Keep::Section.new
+      case args[:type]
+      when :list
+        note.body.list = Keep::ListContent.new
+      when :text
+        note.body.text = Keep::TextContent.new
+      end
+
+      if block_given?
+        yield note
+      else
+        return note
+      end
+    end
+
+    def with_authorized_service 
+      svc = Keep::KeepService.new
+      svc.authorization = @authorizer
+      yield svc
+    end
+  end
+
+  module_function
+
+  def user_credentials(**args)
+      scope = [Keep::AUTH_KEEP, Keep::AUTH_KEEP_READONLY, 'https://www.googleapis.com/auth/userinfo.email']
+
+      authorizer = Google::Auth::ServiceAccountCredentials.make_creds( 
+        json_key_io: File.open(args[:api_key]),
+        scope: scope       
+      )
+      authorizer.update!(sub: args[:for])
+      authorizer.fetch_access_token!
+      return authorizer
+  end
 end
